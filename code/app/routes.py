eb9d3bb356f5d7d30f49206337029e1229411fc6
kangea <eunice.kang.58@gmail.com>
2019-10-17 21:29:06 -0700

    added index.html template

--- code/app/routes.py
+++ code/app/routes.py
@@ -1,6 +1,7 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hi Taehoon and Eunice!!"
+    return render_template('index.html', user="Bill")
