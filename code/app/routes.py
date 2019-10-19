c20fd14043c60251ea4d0f0c61f186af91fbe794
unknown <th.kim9112@gmail.com>
2019-10-18 18:44:04 -0700

    add practive 3 jpg files

--- code/app/routes.py
+++ code/app/routes.py
@@ -1,6 +1,22 @@
 from app import app
+from flask import render_template, Response
+from camera import Camera
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hi Taehoon and Eunice!!"
+    return render_template('index.html')
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(Camera()),
+           mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, threaded=True)
